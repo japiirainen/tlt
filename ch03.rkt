ecb88968844de813443a0a2d9da11687221b7fac
Joona Piirainen <joona.piirainen@gmail.com>
2022-06-14 21:17:55 +0300

    Feat: zerop using rec-Nat

--- ch03.rkt
+++ ch03.rkt
@@ -12,3 +12,17 @@
 (check-same Nat (+ 1 2) 3)
 (check-same Nat (+ 5 2) 7)
 (check-same Nat (+ 5 zero) 5)
+
+; 43
+(claim zerop
+  (-> Nat
+      Atom))
+(define zerop
+  (λ (n)
+    (rec-Nat n
+      't
+      (λ (n-1 zeropn-1)
+        'nil))))
+
+(check-same Atom (zerop zero) 't)
+(check-same Atom (zerop 1) 'nil)
